aaafb89530dccd21d32b2470b3f4ce8ac1f464c8
root <abrielleperry22@icloud.com>
2023-11-18 12:53:43 -0800

    Running on http://0.0.0.0:5000/

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """index"""
-from api.v1.views import app_views
 from flask import Flask
 
 app = Flask(__name__)
 
 @app_views.route("/status")
+from api.v1.views import app_views
 def status():
   response = {"status": "OK"}
-return jsonify(response)
+  return jsonify(response)
