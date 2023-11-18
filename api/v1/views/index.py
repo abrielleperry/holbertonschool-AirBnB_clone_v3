ce180cad98d9b3e4a73e5d0a09f6370eb37f995b
root <abrielleperry22@icloud.com>
2023-11-18 13:09:54 -0800

    pep8 fixes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,8 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 
+
 @app_views.route("/status")
 def status():
-  response = {"status": "OK"}
-  return jsonify(response)
+    response = {"status": "OK"}
+    return jsonify(response)
