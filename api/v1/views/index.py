2f10ab2e4b0f94e6acab815767486ce60116d0cd
Moonwalkert3ch <6620@holbertonstudents.com>
2023-11-19 15:11:36 -0600

    added imports

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,13 @@
 """index"""
 from flask import Flask, jsonify
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import  storage
 
 
 app = Flask(__name__)
