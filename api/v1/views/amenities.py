ef409c4c33a846f1384ca700e04068fe46951711
root <abrielleperry22@icloud.com>
2023-11-20 17:48:00 -0800

    trying to revert back to checks

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -2,6 +2,8 @@
 """ objects that handles all default RestFul API actions for amenities """
 from flask import abort, jsonify, make_response, request
 from models.amenity import Amenity
+from models.state import State
+from models.city import City
 from models import storage
 from api.v1.views import app_views
 
@@ -10,11 +12,9 @@ from api.v1.views import app_views
                  methods=['GET'], strict_slashes=False)
 def get_all_amenities():
     """Retrieves list all state objects"""
-
-
-amenity_list = [amenity.to_dict()
-                for amenity in storage.all(Amenity).values()]
-return jsonify(amenity_list)
+    amenity_list = [amenity.to_dict()
+                    for amenity in storage.all(Amenity(values()))]
+    return jsonify(amenity_list)
 
 
 @app_views.route('/api/v1/amenities/<amenity_id>',
@@ -44,14 +44,11 @@ def post_amenity(amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if not amenity:
         abort(404)
-
     response = request.get_json()
     if not response:
         abort(400, description="Not a JSON")
-
     if "name" not in response:
         return make_response(jsonify({"error": "Missing name"}), 400)
-
     created_amenity = Amenity(**response)
     created_amenity.amenity_id = amenity_id
     created_amenity.save()
@@ -64,15 +61,12 @@ def put_amenity(amenity_id):
     amenity_obj = storage.get(Amenity, amenity_id)
     if not amenity_obj:
         abort(404)
-
     response = request.get_json()
     if not response:
         abort(400, description="Not a JSON")
-
     keys_to_ignore = ["id", "created_at", "updated_at"]
     for key, value in response.items():
         if key not in keys_to_ignore:
             setattr(amenity_obj, key, value)
-
     storage.save()
     return make_response(jsonify(amenity_obj.to_dict()), 200)
