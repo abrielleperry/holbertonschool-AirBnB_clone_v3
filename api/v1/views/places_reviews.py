8f78d12a67da2401ac9e73c376a09857ba0fba4a
Moonwalkert3ch <6620@holbertonstudents.com>
2023-11-21 20:55:46 -0600

    update review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Create a new view for Review object 
+"""Create a new view for Review object
 that handles all default RESTFul API actions"""
 
 from flask import jsonify, abort, make_response, request
@@ -23,6 +23,7 @@ def get_all_reviews(place_id):
                review.place_id == place_id]
     return jsonify(reviews)
 
+
 @app_views.route('/reviews/<review_id>', methods=['GET'],
                  strict_slashes=False)
 def get_review(review_id):
@@ -33,7 +34,8 @@ def get_review(review_id):
 
     return jsonify(review.to_dict())
 
-@app_views.route('/reviews/<review_id>', methods=['GET'],
+
+@app_views.route('/reviews/<review_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_review(review_id):
     review = storage.get(Review, review_id)
@@ -44,7 +46,8 @@ def delete_review(review_id):
     storage.save()
     return make_response(jsonify{}, 200)
 
-@app_views.route('/places/<places_id>/reviews', methods=['GET'],
+
+@app_views.route('/places/<places_id>/reviews', methods=['POST'],
                  strict_slashes=False)
 def post_review(place_id):
     """Creates a Review"""
@@ -56,16 +59,16 @@ def post_review(place_id):
     obj_dict = request.get_json()
     if not obj_dict:
         return make_response(jsonify({"error": "Not a JSON"}), 400)
-    
+
     user_id = obj_dict.get('user_id')
     text = obj_dict.get('text')
 
     if not user_id:
         return make_response(jsonify({"error": "Missing user_id"}), 400)
-    
+
     if not text:
         return make_response(jsonify({"error": "Missing text"}), 400)
-    
+
     if not storage.get(User, user_id):
         abort(404)
 
@@ -74,3 +77,26 @@ def post_review(place_id):
     new_review.save()
 
     return make_response(jsonify(new_review.to_dict()), 201)
+
+
+@app_views.route('/reviews/<review_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_review(review_id):
+    """Updates a Review object"""
+    review = storage.get(Review, review_id)
+    if not review:
+        abort(404)
+
+    obj_dict = request.get_json()
+
+    if not obj_dict:
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+
+    ignore_keys = ['id', 'user_id', 'place_id', 'created_at', 'updated_at']
+
+    for key, value in obj_dict.items():
+        if key not in ignore_keys:
+            setattr(review, key, value)
+
+    review.save()
+    return make_response(jsonify(review.to_dict()), 200)
